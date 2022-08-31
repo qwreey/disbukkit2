8c9d7dbc95862f752e632e3f0945e001ad143975
Qwreey <qwreey75@gmail.com>
2022-08-31 22:22:49 +0900

    bugfix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const { Client, GatewayIntentBits, TextChannel, GuildMember } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 const { default: Dokdo } =  require('dokdo')
+const { dokdo } =  require('dokdo')
 const messageHandlerClass = require("./messageHandler") // message update handler
 const fileWatchClass      = require("./fileWatch") // file update handler
 const logformatterClass   = require("./logformatter")
@@ -22,7 +23,7 @@ const /** @type { String } */ commandTellraw   = settings.commandTellraw
 const /** @type { String } */ commandFormatter = settings.commandFormatter
 
 // load dokdo
-if (!settings.disableDokdo) dokdoHandler = new Dokdo(client, settings.dokdoOptions)
+if (!settings.disableDokdo) dokdoHandler = new dokdo(client, settings.dokdoOptions)
 
 /** Escape chat string */
 function safeString(str) {
